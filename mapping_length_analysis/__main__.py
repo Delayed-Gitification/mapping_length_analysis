614ba3f4663108d7b523f5b184afef1386e3af32
Oscar <oscargwilkins@gmail.com>
2021-08-07 12:06:39 +0100

    working

--- mapping_length_analysis/__main__.py
+++ mapping_length_analysis/__main__.py
@@ -31,7 +31,18 @@ def analyse_fq(fq_name):
 def write_dict(d, name, d2={}):
     for_df = {'length': d.keys(), 'n': d.values()}
     df = pd.DataFrame.from_dict(for_df)
-    print(df)
+
+    if len(d2.keys()) > 0:  # if there is bam and fastq
+        for_df2 = {'length': d2.keys(), 'n_bam': d2.values()}
+        df2 = pd.DataFrame.from_dict(for_df2)
+        df = pd.merge(df, df2, how="outer", on="length").fillna(0)
+        df['fraction_aligned'] = df['n_bam'] / df['n']
+    
+    df = df.sort_values(by=['length'])
+    print("writing to "+ name + ".csv")
+    df.to_csv(name+'.csv', index=False)
+
+
 
 
 def main():
@@ -49,11 +60,13 @@ def main():
     assert n_inputs > 0, "Provide at least one input file!"
 
     if args.bam != "None":
+        print("Reading bam file")
         bam_stats = analyse_bam(args.bam)
         if n_inputs == 1:
             write_dict(bam_stats, args.output)
 
     if args.fastq != "None":
+        print("Reading fastq file")
         fq_stats = analyse_fq(args.fastq)
         if n_inputs == 1:
             write_dict(fq_stats, args.output)
@@ -64,4 +77,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
+
+# python3 __main__.py -f ~/Desktop/mapping_length_analysis/test_data/ultraplex_demux_fmrp1.fastq.gz -b ~/Desktop/mapping_length_analysis/test_data/fmrp1.Aligned.sortedByCoord.out.bam -o fmrp1
